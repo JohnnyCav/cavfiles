5ec05565a11f0a3d435d32ec34c50a9cf869c0e3
Johnny Cav <joaokavaleiro@gmail.com>
2022-04-10 22:31:10 +0100

    Update cavfiles

--- install_i3cav.sh
+++ install_i3cav.sh
@@ -6,19 +6,16 @@ echo "Starting i3cav installer..."
 sudo dnf install i3-gaps --allowerasing -y
 
 # install other dependencies
-sudo dnf install zsh i3status i3lock rofi alacritty picom nitrogen polkit-gnome lxappearance nautilus dunst volumeicon network-manager-applet arandr blueman pavucontrol pactl xbacklight ImageMagick -y
+sudo dnf install zsh i3status i3lock rofi alacritty picom nitrogen polkit-gnome lxappearance nautilus dunst volumeicon network-manager-applet arandr blueman pavucontrol xbacklight ImageMagick -y
 
 # move config files around
 mv alacritty ~/.config/
 mv dunst ~/.config/
-
 mv i3 ~/.config/
 mv i3status ~/.config/
-mv alacritty ~/.config/
-mv alacritty ~/.config/
-mv alacritty ~/.config/
-mv alacritty ~/.config/
-
+mv nitrogen ~/.config/
+mv rofi ~/.config/
+mv volumeicon ~/.config/
 
 # themes, icons and fonts
 mkdir ~/.themes 
