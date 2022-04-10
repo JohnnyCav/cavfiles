0cc58ed721af58c236e46d80fe98cd66a00a425e
Johnny Cav <joaokavaleiro@gmail.com>
2022-04-10 22:40:52 +0100

    Update cavfiles

--- install_i3cav.sh
+++ install_i3cav.sh
@@ -9,39 +9,39 @@ sudo dnf install i3-gaps --allowerasing -y
 sudo dnf install zsh i3status i3lock rofi alacritty picom nitrogen polkit-gnome lxappearance nautilus dunst volumeicon network-manager-applet arandr blueman pavucontrol xbacklight ImageMagick -y
 
 # move config files around
-rm -rf .config/alacritty
-mv alacritty ~/.config/
+rm -rf ~/.config/alacritty
+mv ~/cavfiles/alacritty ~/.config/
 
-rm -rf .config/dunst
-mv dunst ~/.config/
+rm -rf ~/.config/dunst
+mv ~/cavfiles/dunst ~/.config/
 
-rm -rf .config/i3
-mv i3 ~/.config/
+rm -rf ~/.config/i3
+mv ~/cavfiles/i3 ~/.config/
 
-rm -rf .config/i3status
-mv i3status ~/.config/
+rm -rf ~/.config/i3status
+mv ~/cavfiles/i3status ~/.config/
 
-rm -rf .config/nitrogen
-mv nitrogen ~/.config/
+rm -rf ~/.config/nitrogen
+mv ~/cavfiles/nitrogen ~/.config/
 
-rm -rf .config/rofi
-mv rofi ~/.config/
+rm -rf ~/.config/rofi
+mv ~/cavfiles/rofi ~/.config/
 
-rm -rf .config/volumeicon
-mv volumeicon ~/.config/
+rm -rf ~/.config/volumeicon
+mv ~/cavfiles/volumeicon ~/.config/
 
 # themes, icons and fonts
 mkdir ~/.themes 
 mkdir ~/.icons
 
 mv fonts ~/.local/share/
-mv -R icons/* ~/.icons
-mv -R themes/* ~/.themes
+mv -i ~/cavfiles/icons/* ~/.icons
+mv -i ~/cavfiles/themes/* ~/.themes
 
 # install papirus icons
 sudo dnf install papirus-icon-theme -y
 
 # move wallpapers
-mv -R wallpapers/* ~/Pictures/
+mv -i ~/cavfiles/wallpapers/* ~/Pictures/
 
 echo "i3cav finish installing!"
