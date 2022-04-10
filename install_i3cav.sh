bc0e388b7769b2b0ebf3a01672fea9eab93cdfd3
Johnny Cav <joaokavaleiro@gmail.com>
2022-04-10 22:07:06 +0100

    Create install script for i3 fedora

---
+++ install_i3cav.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+echo "Starting i3cav installer..."
+
+# install i3-gaps instead of i3
+sudo dnf install i3-gaps --allowerasing -y
+
+# install other dependencies
+sudo dnf install zsh i3status i3lock rofi alacritty picom nitrogen polkit-gnome lxappearance nautilus dunst volumeicon network-manager-applet arandr blueman pavucontrol pactl xbacklight ImageMagick -y
+
+# move config files around
+mv alacritty ~/.config/
+mv dunst ~/.config/
+
+mv i3 ~/.config/
+mv i3status ~/.config/
+mv alacritty ~/.config/
+mv alacritty ~/.config/
+mv alacritty ~/.config/
+mv alacritty ~/.config/
+
+
+# themes, icons and fonts
+mkdir ~/.themes 
+mkdir ~/.icons
+
+mv fonts ~/.local/share/
+mv -R icons/* ~/.icons
+mv -R themes/* ~/.themes
+
+# move wallpapers
+mv -R wallpapers/* ~/Pictures/
+
+echo "i3cav finish installing!"
--- vpn.sh
+++
@@ -1,2 +0,0 @@
-dns_ip=$(sudo barracudavpn --status | grep -P '^D(?:NS|ns)[v4]*:.*[0-9]+.[0-9]+.[0-9]+.[0-9]+' | awk '{print $2}')
-sudo systemd-resolve -i tun0 --set-dns=$dns_ip --set-domain=~. --set-domain=critical.pt --set-dnssec=off
